5b96b6a3f24d60c0c810b61fb0de3e9157bd8710
[Wasim Akhtar Khan] <wasimakhtarkhan2003@gmail.com>
2025-09-25 23:29:26 +0530

    Project Structure and Styling Import Done

---
+++ components/blog/fonts.ts
@@ -0,0 +1,3 @@
+import {Inter,Outfit} from "next/font/google";
+export const inter= Inter({ subsets: ["latin"] , weight: ['400', '700']});
+export const outfit= Outfit({ subsets: ["latin"] , weight: ['400', '700']});
